9765f6964c9601b427b38fd3e1035a97ab2feb84
Pedro H. Windisch <pedrowindisch@gmail.com>
2020-06-09 21:07:41 -0300

    Initial Dialog component files.

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+/*import { Dialog } from '@reach/dialog';
+
+const StyledDialog = styled(Dialog)`
+  position: absolute;
+  top: 0;
+  padding: 2rem;
+  background-color: #C3C3C3;
+`;
+
+const CustomDialog: React.FC<any> = (props) => {
+  return (
+    <StyledDialog isOpen={true}>
+      <h1>Test</h1>
+    </StyledDialog>
+  );
+};*/
+
+export default (props: any) => {
+  return ReactDOM.createPortal(
+    <dialog {...props} />,
+    document.body
+  );
+};
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,16 +3,17 @@ import LoadingStream from "./LoadingStream/LoadingStream";
 import Paragraph from './Paragraph/Paragraph';
 import Sidebar from './Sidebar/Siderbar';
 import Button from './Button/Button';
+import Dialog from './Dialog/Dialog';
 import Title from './Title/Title';
 import Input from './Input/Input';
 import Video from './Video/Video';
-
 
 export {
   OnlyAudioStream,
   LoadingStream,
   Paragraph,
   Sidebar,
+  Dialog,
   Button,
   Title,
   Input,
