437a742f15c4901a4011185b2fc67872d24f3d93
Pedro H. Windisch <pedrowindisch@gmail.com>
2020-06-13 21:15:33 -0300

    Added flip orientation feature.

--- src/pages/Room/components/RoomVideos.tsx
+++ src/pages/Room/components/RoomVideos.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
 
 type TOrientation = 'horizontal' | 'vertical'; 
 
 const Wrapper = styled.div<any>`
+  position: relative;
   grid-row: 1 / 2;
   width: 100%;
   height: 100%;
@@ -12,11 +14,37 @@ const Wrapper = styled.div<any>`
   grid-template-rows: ${props => props.orientation === 'horizontal' ? '50% 50%' : '100%'};
 `;
 
+const FlipOrientationIcon = styled(FontAwesomeIcon)`
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  color: white;
+  top: 10px;
+  left: 10px;
+  z-index: 20;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 const RoomVideos: React.FC<any> = ({ children }) => {
   const [orientation, setOrientation] = useState<TOrientation>('vertical');
 
+  const handleIconClick = () => {
+    setOrientation(orientation === 'horizontal' ? 'vertical' : 'horizontal');
+  };
+
   return (
     <Wrapper orientation={orientation}>
+      { children.length <= 2 && (
+          <FlipOrientationIcon
+            tabIndex={0}
+            icon="sync-alt"
+            onClick={handleIconClick}
+            onKeyDown={handleIconClick} 
+          /> 
+      )}
       { children }
     </Wrapper>
   )
